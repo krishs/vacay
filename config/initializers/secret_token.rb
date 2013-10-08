12905300b062c26767613873595570bf441e15c8
Ram Poornalingam <rampos@gmail.com>
2013-10-08 13:28:31 -0700

    first checkin

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Vacay::Application.config.secret_key_base = '4f33a12a625f56385c4d1f51a32b2301e234e9a8e2f571be7f4adb471729d9f8f85d1fbf2f17e93d6ec65976930e373ffbb957e3de80421172ece8c90c218330'
